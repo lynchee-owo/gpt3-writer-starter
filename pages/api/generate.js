b805341ce60786b7d710e57e581be8209e2e2f2d
lynchee-owo <lynchee.owo@gmail.com>
2023-03-07 13:18:32 -0500

    latset build with customization

---
+++ pages/api/generate.js
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "Create 10 catching titles for the following: ";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  const secondPrompt = 
+  `Take these ten titles: 
+    ${basePromptOutput.text} and generate 10 new titles on ${req.body.userInput} with more diverse styles.
+  `
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
